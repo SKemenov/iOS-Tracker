1e9acc4a58d04110c128693534cc17999bb38b34
Sergey Kemenov <skemenov@icloud.com>
2023-10-24 23:08:48 +0300

    Added `TrackerHeader` class for collectionView's headers

---
+++ Tracker-ios/Views/TrackerHeader.swift
@@ -0,0 +1,41 @@
+//
+//  TrackerHeader.swift
+//  Tracker-ios
+//
+//  Created by Sergey Kemenov on 24.10.2023.
+//
+
+import UIKit
+
+final class TrackerHeader: UICollectionReusableView {
+  let titleLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.textColor = .ypBlack
+    label.textAlignment = .natural
+    label.backgroundColor = .ypRed
+    label.font = Resources.Fonts.sectionHeader
+    return label
+  }()
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureUI()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+private extension TrackerHeader {
+  func configureUI() {
+    addSubview(titleLabel)
+
+    NSLayoutConstraint.activate([
+      titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Resources.Layouts.leadingTracker),
+      titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Resources.Layouts.vSpacingSection),
+      titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Resources.Layouts.vSpacingTracker)
+    ])
+  }
+}
