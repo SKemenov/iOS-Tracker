a234f6c591f6bf20fe1105884ebf4573cd6cc633
Sergey Kemenov <skemenov@icloud.com>
2023-10-24 23:11:55 +0300

    Refactored `TrackersFactory` class: - made properties private - added `addTracker()`, `addNewTracker`, `addNewCategory`, `removeTrackerWithIndex`, `removeCategoryWithIndex` methods to operate data in private properties

--- Tracker-ios/Services/TrackersFactory.swift
+++ Tracker-ios/Services/TrackersFactory.swift
@@ -10,8 +10,39 @@ import Foundation
 final class TrackersFactory {
   static let shared = TrackersFactory()
 
-  var trackers: [Tracker] = []
-  var categories: [TrackerCategory] = []
+  private (set) var trackers: [Tracker] = []
+  private (set) var categories: [TrackerCategory] = []
 
   private init() { }
+
+  func addTracker(_ tracker: Tracker, toCategory index: Int) {
+    var currentCategories = categories
+    var currentCategory = currentCategories[index]
+    var categoryTrackers = currentCategory.items
+    categoryTrackers.append(tracker)
+    var updatedCategory = TrackerCategory(
+      id: currentCategory.id,
+      name: currentCategory.name,
+      items: categoryTrackers
+    )
+    currentCategories.remove(at: index)
+    currentCategories.append(updatedCategory)
+    categories = currentCategories
+  }
+
+  func addNew(tracker: Tracker) {
+    trackers.append(tracker)
+  }
+
+  func addNew(category: TrackerCategory) {
+    categories.append(category)
+  }
+
+  func removeTrackerWith(index: Int) {
+    trackers.remove(at: index)
+  }
+
+  func removeCategoryWith(index: Int) {
+    categories.remove(at: index)
+  }
 }
