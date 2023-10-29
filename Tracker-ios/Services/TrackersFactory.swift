896cdc64a853811a757f439cd7cac8027ca5d33b
Sergey Kemenov <skemenov@icloud.com>
2023-10-29 15:02:02 +0300

    Refactored `TrackersFactory` class: - made `addNewTracker` method private - added methods to record completed trackers

--- Tracker-ios/Services/TrackersFactory.swift
+++ Tracker-ios/Services/TrackersFactory.swift
@@ -18,6 +18,8 @@ final class TrackersFactory {
 
   func addTracker(_ tracker: Tracker, toCategory index: Int) {
     print("TF Run addTracker()")
+    addToFactoryNew(tracker: tracker)
+    addToCompletedNew(tracker: tracker)
     var currentCategories = categories
     let currentCategory = currentCategories[index]
     var categoryTrackers = currentCategory.items
@@ -30,40 +32,48 @@ final class TrackersFactory {
     currentCategories.remove(at: index)
     currentCategories.append(updatedCategory)
     categories = currentCategories
+//    print("TF trackers \(trackers)")
+//    print("TF categories \(categories)")
+//    print("TF completedTrackers \(completedTrackers)")
   }
 
-  func addNew(tracker: Tracker) {
-    trackers.append(tracker)
-  }
 
   func addNew(category: TrackerCategory) {
     categories.append(category)
   }
 
-  func removeTrackerWith(index: Int) {
-    trackers.remove(at: index)
-  }
-
-  func removeCategoryWith(index: Int) {
-    categories.remove(at: index)
-  }
-
-  func addDayToTracker(with id: UUID, for date: Date) {
-    print("TF Run addDayToTracker()")
+  func setTrackerDone(with id: UUID, on date: Date) -> (Int, Bool) {
+    print("TF Run isTrackerDone()")
+    var isCompleted = false
     var currentCompletedTrackers = completedTrackers
     print("TF currentCompletedTrackers \(currentCompletedTrackers)")
-    let index = fetchCompletedTrackerIndex(by: id)
+    let index = findInCompletedTrackerIndex(by: id)
     print("TF currentCompletedTrackers \(currentCompletedTrackers)")
     print("TF index \(index)")
     let currentCompletedTracker = currentCompletedTrackers[index]
+
+    print("TF Try to catch day \(date) in Dates \(currentCompletedTracker.dates)")
+    let indexDate = currentCompletedTracker.dates.firstIndex { $0 == date }
+    print("TF isTrackerDone() has index \(String(describing: indexDate))")
+
     print("TF currentCompletedTracker \(currentCompletedTracker)")
-    var newDates = currentCompletedTracker.date
-    newDates.append(date)
+    var newDates = currentCompletedTracker.dates
+    if indexDate == nil {
+      newDates.append(date)
+      print("TF Trying to add new date \(date)")
+      isCompleted = true
+    } else {
+      guard let indexDate else {
+        preconditionFailure("Cannot obtain the index")
+      }
+      newDates.remove(at: indexDate)
+      print("TF Trying to remove date at index \(indexDate)")
+    }
     print("TF newDates \(newDates)")
     let updatedCompletedTracker = TrackerRecord(
       id: currentCompletedTracker.id,
       tracker: currentCompletedTracker.tracker,
-      date: newDates
+      dates: newDates
     )
     print("TF currentCompletedTrackers before remove \(currentCompletedTrackers)")
     currentCompletedTrackers.remove(at: index)
@@ -72,39 +82,56 @@ final class TrackersFactory {
     print("TF currentCompletedTrackers final \(currentCompletedTrackers)")
     completedTrackers = currentCompletedTrackers
     print("TF completedTrackers \(completedTrackers)")
+
+    let counter = countDays(for: index)
+    print("TF countDays \(String(describing: countDays))")
+    return (counter, isCompleted)
   }
 
-  func addNewCompleted(tracker: Tracker) {
+  func getCounter(with id: UUID, on date: Date) -> (Int, Bool) {
+    let index = findInCompletedTrackerIndex(by: id)
+    let trackerDates = completedTrackers[index].dates
+    guard trackerDates.firstIndex(where: { $0 == date }) != nil else {
+      return (countDays(for: index), false)
+    }
+    return (countDays(for: index), true)
+  }
+}
+
+// MARK: - Private methods
+
+private extension TrackersFactory {
+  func addToFactoryNew(tracker: Tracker) {
+    trackers.append(tracker)
+  }
+
+  func addToCompletedNew(tracker: Tracker) {
     print("TF Run addNewCompleted()")
-    completedTrackers.append(TrackerRecord(id: UUID(), tracker: tracker, date: []))
+    completedTrackers.append(TrackerRecord(id: UUID(), tracker: tracker, dates: []))
   }
 
-  func findTrackerIndex(by id: UUID) -> Tracker {
+  func countDays(for index: Int) -> Int {
+    let trackerRecord = completedTrackers[index]
+    let dates = trackerRecord.dates.count
+    return dates
+  }
+
+  func findInFactoryTrackerIndex(by id: UUID) -> Int {
     print("TF Run findTrackerIndex()")
     guard let index = trackers.firstIndex(where: { $0.id == id }) else {
-      preconditionFailure("error")
+      preconditionFailure("Cannot obtain the index")
     }
-    return trackers[index]
-  }
-
-  func findIndexForCompleted(_ tracker: Tracker) -> Int {
-    print("TF Run findIndexForCompleted()")
-    guard let index = completedTrackers.firstIndex(where: { $0.tracker == tracker }) else {
-      preconditionFailure("error")
-    }
+    print("TF Find index of trackers[\(index)] - \(trackers[index])")
     return index
   }
 
-  func isCompleted(_ tracker: Tracker) -> Bool {
-    completedTrackers.contains { $0.tracker == tracker }
-  }
-
-  func fetchCompletedTrackerIndex(by id: UUID) -> Int {
+  func findInCompletedTrackerIndex(by id: UUID) -> Int {
     print("TF Run fetchCompletedTrackerIndex()")
-    let tracker = findTrackerIndex(by: id)
-    if !isCompleted(tracker) {
-      addNewCompleted(tracker: tracker)
+    let tracker = trackers[findInFactoryTrackerIndex(by: id)]
+    guard let index = completedTrackers.firstIndex(where: { $0.tracker == tracker }) else {
+      preconditionFailure("Cannot obtain the index")
     }
-    return findIndexForCompleted(tracker)
+    print("TF Find index of completedTrackers[\(index)] - \(completedTrackers[index])")
+    return index
   }
 }
