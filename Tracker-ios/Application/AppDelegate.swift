ced664d0741a0f8aa9a9090fa69e224cd141df48
Sergey Kemenov <skemenov@icloud.com>
2023-11-07 00:38:50 +0300

    Refactored `Tracker` coreData model

--- Tracker-ios/Application/AppDelegate.swift
+++ Tracker-ios/Application/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   // MARK: - Core Data stack
 
   lazy var persistentContainer: NSPersistentContainer = {
-    let container = NSPersistentContainer(name: "demo")
+    let container = NSPersistentContainer(name: "Tracker")
     // swiftlint:disable:next trailing_closure unused_closure_parameter
     container.loadPersistentStores(completionHandler: { storeDescription, error in
       if let error = error as NSError? {
