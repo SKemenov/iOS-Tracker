5d90ed937e0ce787696cd525f8196e1b27c60257
Sergey Kemenov <skemenov@icloud.com>
2023-11-12 13:27:36 +0300

    Commented all `ScheduleMarshalling` and `TrackersFactory` classes. If necessarily delete files before PR

--- Tracker-ios/Services/ScheduleMarshalling.swift
+++ Tracker-ios/Services/ScheduleMarshalling.swift
@@ -5,22 +5,22 @@
 //  Created by Sergey Kemenov on 05.11.2023.
 //
 
-import Foundation
-
-// MARK: - Class
-
-final class ScheduleMarshalling {
-  // MARK: - Public methods
-
-  func makeArray(from string: String) -> [Bool] {
-    var array: [Bool] = []
-    string.forEach { array.append($0 == "1" ? true : false) }
-    return array
-  }
-
-  func makeString(from schedule: [Bool]) -> String {
-    var scheduleToString: [String] = []
-    schedule.forEach { scheduleToString.append($0 == true ? "1" : "0") }
-    return scheduleToString.joined()
-  }
-}
+// import Foundation
+//
+// // MARK: - Class
+//
+// final class ScheduleMarshalling {
+//  // MARK: - Public methods
+//
+//  func makeArray(from string: String) -> [Bool] {
+//    var array: [Bool] = []
+//    string.forEach { array.append($0 == "1" ? true : false) }
+//    return array
+//  }
+//
+//  func makeString(from schedule: [Bool]) -> String {
+//    var scheduleToString: [String] = []
+//    schedule.forEach { scheduleToString.append($0 == true ? "1" : "0") }
+//    return scheduleToString.joined()
+//  }
+// }
