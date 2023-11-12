ecba743647593422fa33d4a3e074363040aa231a
Sergey Kemenov <skemenov@icloud.com>
2023-11-12 11:07:40 +0300

    Refactored `TrackerRecordStore` class: - added `addNew(recordDate date:, toTracker:)`, `removeRecord(onDate:)`, `countRecords(forTracker:)`,  `fetchRecords(forTracker:)` methods for factory. - added `isTrackerRecordCoreDataEmpty`, `showTrackerRecordsFromCoreData` temporarily support methods. Need to delete before PR. - added support print into `saveContext` method

--- Tracker-ios/Services/TrackerRecordStore.swift
+++ Tracker-ios/Services/TrackerRecordStore.swift
@@ -5,8 +5,10 @@
 //  Created by Sergey Kemenov on 04.11.2023.
 //
 
-import UIKit
+import UIKit.UIApplication
 import CoreData
+
+// MARK: - Class
 
 final class TrackerRecordStore {
   // MARK: - Private properties
@@ -24,20 +26,109 @@ final class TrackerRecordStore {
 
   init(context: NSManagedObjectContext) {
     self.context = context
+    if isTrackerRecordCoreDataEmpty() { // TODO: - delete before PR
+      print("TRS - no TrackerRecords in CoreData")
+    } else {
+      showTrackerRecordsFromCoreData()
+    }
   }
 
-  func addNew(record: TrackerRecord) throws { }
+  func addNew(recordDate date: Date, toTracker tracker: TrackerCoreData) throws {
+    print("TRS Run addNew(recordDate:toTracker:)")
+    print(date)
+    print(tracker)
+    let trackerRecordInCoreData = TrackerRecordCoreData(context: context)
+    print(trackerRecordInCoreData)
+    trackerRecordInCoreData.date = date
+    trackerRecordInCoreData.tracker = tracker
+    print("trackerRecordInCoreData: Trying to add date [\(date)] to tracker - \(String(describing: tracker.title))")
+    print(trackerRecordInCoreData)
+    saveContext()
+  }
+
+  func removeRecord(on date: Date) {
+    print("TRS Run remove(trackerRecord:)")
+    let calendar = Calendar.current
+    let request = TrackerRecordCoreData.fetchRequest()
+    guard let records = try? context.fetch(request) else { return }
+    records.forEach { record in
+      guard let recordDate = record.date else { return }
+      if calendar.compare(recordDate, to: date, toGranularity: .day) == .orderedSame {
+        print("Deleting record: \(String(describing: record))")
+        context.delete(record)
+      }
+    }
+    saveContext()
+  }
+
+  func countRecords(for tracker: TrackerCoreData) -> Int {
+    print("TRS Run countRecords(forTracker:)")
+    return fetchRecords(for: tracker).count
+    //    let request = TrackerRecordCoreData.fetchRequest()
+    //    request.resultType = .countResultType
+    //    request.predicate = NSPredicate(format: "%K == %@", #keyPath(TrackerRecordCoreData.tracker), tracker)
+    //    guard
+    //      let objects = try? context.execute(request) as? NSAsynchronousFetchResult<NSFetchRequestResult>,
+    //      let counter = objects.finalResult?[0] as? Int32
+    //    else {
+    //      return .zero
+    //    }
+    //    return Int(counter)
+  }
+
+  func fetchRecords(for tracker: TrackerCoreData) -> [Date] {
+    print("TRS Run fetchRecords(forTracker:)")
+    var dates: [Date] = []
+    let request = TrackerRecordCoreData.fetchRequest()
+    request.returnsObjectsAsFaults = false
+    guard let records = try? context.fetch(request) else { return dates }
+    for record in records where record.tracker == tracker {
+      guard let date = record.date else { break }
+      dates.append(date)
+    }
+    return dates
+  }
+
+  private func isTrackerRecordCoreDataEmpty() -> Bool { // TODO: - delete before PR
+    print("TRS Run isTrackerRecordCoreDataEmpty()")
+    let checkRequest = TrackerRecordCoreData.fetchRequest()
+    guard
+      let result = try? context.fetch(checkRequest),
+      result.isEmpty
+    else {
+      print("isTrackerRecordCoreDataEmpty = false")
+      return false
+    }
+    print("isTrackerRecordCoreDataEmpty = true")
+    return true
+  }
+
+  private func showTrackerRecordsFromCoreData() { // TODO: - delete before PR
+    print("TRS Run showTrackerRecordsFromCoreData()")
+    let request = TrackerRecordCoreData.fetchRequest()
+    request.returnsObjectsAsFaults = false
+    let records = try? context.fetch(request)
+    records?.enumerated().forEach { index, record in
+      guard
+        let date = record.date,
+        let trackerTitle = record.tracker?.title
+      else { return }
+      print("record[\(index)] detailed information")
+      print("record's date: \(date)")
+      print("record's trackerTitle: \(trackerTitle)")
+    }
+  }
 }
 
 
 // MARK: - Core Data Saving support
 
 private extension TrackerRecordStore {
-
   func saveContext() {
     if context.hasChanges {
       do {
         try context.save()
+        print("TRS - content has changed and successfully saved")
       } catch {
         let nserror = error as NSError
         fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
