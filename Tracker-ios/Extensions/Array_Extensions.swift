f8835fb36378ced3ba5d6347b8eab76b4ef4ef66
Sergey Kemenov <skemenov@icloud.com>
2023-12-04 02:09:35 +0300

    Added extension for Arrays to shift them

---
+++ Tracker-ios/Extensions/Array_Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Array_Extensions.swift
+//  Tracker-ios
+//
+//  Created by Sergey Kemenov on 04.12.2023.
+//
+
+import Foundation
+
+extension Array {
+  /**
+   Returns a new array with the first elements up to specified distance being shifted to the end of the collection. If the distance is negative, returns a new array with the last elements up to the specified absolute distance being shifted to the beginning of the collection.
+
+   If the absolute distance exceeds the number of elements in the array, the elements are not shifted.
+   */
+  func shift(withDistance distance: Int = 1) -> Array<Element> {
+    let offsetIndex = distance >= 0 ?
+    self.index(startIndex, offsetBy: distance, limitedBy: endIndex) :
+    self.index(endIndex, offsetBy: distance, limitedBy: startIndex)
+
+    guard let index = offsetIndex else { return self }
+    return Array(self[index ..< endIndex] + self[startIndex ..< index])
+  }
+}
